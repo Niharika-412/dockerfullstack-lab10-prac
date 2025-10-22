c6917aa858595733bcd74ed9b25e8102198f099d
Niharika <nihasrinu.k@gmail.com>
2025-10-22 18:13:24 +0530

    updated

--- Recipe-Manager/src/config.js
+++ Recipe-Manager/src/config.js
@@ -1,10 +1,5 @@
-// src/config.js
-// This file holds your backend API base URL.
-// You can change it later when deploying to another server.
-
 const config = {
-  // Make sure this matches your backend Tomcat base path
-  url: "http://localhost:2030/RecipeReact/api"
-};
+    url: "http://localhost:2030/RecipeReact"
+}
 
 export default config;
